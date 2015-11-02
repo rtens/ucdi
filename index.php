cac552287a6e93d9220d7e74f5bbaec11fcea244
Nikolas Martens <Nikolas.M@rtens.org>
2015-11-02 16:58:35 +0100

    Moved bootstrapping from index.php to Application

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php
 
-use rtens\domin\delivery\web\root\IndexResource;
-use rtens\domin\delivery\web\WebApplication;
-use rtens\domin\reflection\GenericObjectAction;
-use rtens\mockster\Mockster;
-use rtens\ucdi\app\ApplicationService;
-use rtens\ucdi\app\Calendar;
-use rtens\ucdi\app\commands\AddTask;
-use rtens\ucdi\app\commands\CreateGoal;
-use rtens\ucdi\app\commands\ScheduleBrick;
-use rtens\ucdi\es\CommandHandler;
-use rtens\ucdi\es\UidGenerator;
-use watoki\curir\WebDelivery;
+use rtens\ucdi\app\Application;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$handler = new CommandHandler(new ApplicationService(new UidGenerator(), Mockster::mock(Calendar::class)));
-
-$addCommand = function (WebApplication $app, $commandClass) use ($handler) {
-    $app->actions->add((new ReflectionClass($commandClass))->getShortName(), new GenericObjectAction($commandClass, $app->types, $app->parser,
-        function ($command) use ($handler) {
-            return $handler->handle($command);
-        }));
-};
-
-WebDelivery::quickResponse(IndexResource::class, WebApplication::init(function (WebApplication $app) use ($addCommand) {
-    $addCommand($app, CreateGoal::class);
-    $addCommand($app, AddTask::class);
-    $addCommand($app, ScheduleBrick::class);
-}, WebDelivery::init()));
+Application::run();
---
+++ src/app/Application.php
@@ -0,0 +1,33 @@
+<?php namespace rtens\ucdi\app;
+
+use rtens\domin\delivery\web\root\IndexResource;
+use rtens\domin\delivery\web\WebApplication;
+use rtens\domin\reflection\GenericObjectAction;
+use rtens\mockster\Mockster;
+use rtens\ucdi\app\commands\AddTask;
+use rtens\ucdi\app\commands\CreateGoal;
+use rtens\ucdi\app\commands\ScheduleBrick;
+use rtens\ucdi\es\CommandHandler;
+use rtens\ucdi\es\UidGenerator;
+use watoki\curir\WebDelivery;
+
+class Application {
+
+    public static function run() {
+        $handler = new CommandHandler(new ApplicationService(new UidGenerator(), Mockster::mock(Calendar::class)));
+
+        $addCommand = function (WebApplication $app, $commandClass) use ($handler) {
+            $app->actions->add((new \ReflectionClass($commandClass))->getShortName(), new GenericObjectAction($commandClass, $app->types, $app->parser,
+                function ($command) use ($handler) {
+                    return $handler->handle($command);
+                }));
+        };
+
+        WebDelivery::quickResponse(IndexResource::class, WebApplication::init(function (WebApplication $app) use ($addCommand) {
+            $addCommand($app, CreateGoal::class);
+            $addCommand($app, AddTask::class);
+            $addCommand($app, ScheduleBrick::class);
+        }, WebDelivery::init()));
+    }
+
+}
